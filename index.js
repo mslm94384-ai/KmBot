1da59eb0ae89f14aae1d3d4638416e461e11d5d3
tast53534-svg <tast53534@gmail.com>
2026-05-10 07:13:36 +0300

    1index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '584163955749', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [

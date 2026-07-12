f3e03545ae5ad697151a06af04281dc02a3476fa
mslm94384-ai <mslm94384@gmail.com>
2026-07-12 04:41:32 +0300

    Update OpenAi.js

--- plugins/ai/OpenAi.js
+++ plugins/ai/OpenAi.js
@@ -1,11 +1,22 @@
 const handler = async (m, { conn, text, bot }) => {
-if (!text) return m.reply("💙 ~ حط نص جنب الأمر ~ ❤️")
+if (!text) return m.reply("⚔️ ~ حط نص جنب الأمر يا جندي ~ 🔥")
 const { api } = bot.config.info.urls
 const url = api + `/home/sections/Ai/api/Ai/CustomPrompt?q=${text}`;
 const response = await fetch(url)
 const { data } = await response.json()
 
-m.reply(data)
+await conn.sendMessage(m.chat, {
+    text: data,
+    contextInfo: {
+        isForwarded: true,
+        forwardingScore: 1,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '0029VbCoE0P8aKvPbZf8hU1D@newsletter',
+            newsletterName: '𝐄𝐒𝟕 🧛',
+            serverMessageId: 0
+        }
+    }
+}, { quoted: m })
 };
 
 handler.usage = ["اوبن"];

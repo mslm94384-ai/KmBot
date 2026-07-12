01944d5b83c412f2d402338c9175206e6bc1c1a8
mslm94384-ai <mslm94384@gmail.com>
2026-07-12 04:22:12 +0300

    Update delete.js

--- plugins/admins/delete.js
+++ plugins/admins/delete.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn }) => {
-  if (!m.quoted) return m.reply(" 🌹 - رد علي الرساله يا ذكي ")
+  if (!m.quoted) return m.reply("✨ رد ع الرسالة يفنان ✨")
   m.quoted.delete()
  // m.delete()
 };
